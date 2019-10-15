86eb81a175c5c818f4d1467331692de58d04881a
Michael Carroll <carrollm2@gmail.com>
2019-10-15 02:46:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,11 +5,11 @@ def oxford_comma(array)
 
   array.each do |item|
 
-    if array.at[last_index] and array.length > 2
+    if array.at(last_index) and array.length > 2
       converterd_string += ", and "
-    elsif array.at[last_index] and array.length == 2
+    elsif array.at(last_index) and array.length == 2
       converted_string += " and "
-    elsif array.at[0]
+    elsif array.at(0)
       converted_string += "#{item}"
     else
       converted_string += ", #{item}"
